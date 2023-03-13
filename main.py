d030b13c5fe6ef4a83c8551f731f863a81bb6d85
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-13 11:31:45 +0500

    replacing wrong values

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ df['Date'] = pd.to_datetime(df['Date'])
 # removing the row with empty/null value
 df.dropna(subset=['Date'], inplace=True)
 
+# replacing wrong value
+df.loc[7, 'Duration'] = 45
+
 print(df.to_string())
+
+
