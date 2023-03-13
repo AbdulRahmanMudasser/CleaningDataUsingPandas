bc491dabe3699e008059c5067f13a87d138a00ae
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-13 09:36:54 +0500

    cleaning data of wrong format

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+# reading csv file
 df = pd.read_csv('venv/resource/DataSet.csv')
 
-print(df)
+# convert data into a correct format
+df['Date'] = pd.to_datetime(df['Date'])
+
+# removing the row with empty/null value
+df.dropna(subset=['Date'], inplace=True)
+
+print(df.to_string())
