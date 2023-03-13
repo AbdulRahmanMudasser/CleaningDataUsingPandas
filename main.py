1009d922c6678398c61dcaa969e46b11b55ca2e2
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-13 20:20:45 +0500

    reading data

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import pandas as pd
 # reading csv file
 df = pd.read_csv('venv/resource/DataSet.csv')
 
-# convert data into a correct format
-df['Date'] = pd.to_datetime(df['Date'])
+# read each column
+print(df['Calories'])
+print(df['Calories'][0:5])
+print(df[['Calories', 'Duration']])
 
-# removing the row with empty/null value
-df.dropna(subset=['Date'], inplace=True)
+# # read headers
+# print(df.columns)
 
-# replacing wrong value
-df.loc[7, 'Duration'] = 45
-
-print(df.to_string())
-
-
+# # convert data into a correct format
+# df['Date'] = pd.to_datetime(df['Date'])
+#
+# # removing the row with empty/null value
+# df.dropna(subset=['Date'], inplace=True)
+#
+# # replacing wrong value
+# df.loc[7, 'Duration'] = 45
+#
+# print(df.to_string())
