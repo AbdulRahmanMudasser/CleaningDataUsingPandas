89fc397d638165901b7776f9379be6aa225fd7ca
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-14 09:31:41 +0500

    gutenberg from nltk.corpus

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import gutenberg
 
-message = "I will walk 500 miles and I would walk 500 more, " \
-       "just to be the man who walks a thousand miles to fall down at your door!"
+hamlet = gutenberg.words()
 
-# tokenization
-print(word_tokenize(message))
-print(sent_tokenize(message))
+for word in hamlet[0:500]:
+    print(word, sep=' ', end=' ')
+
+# from nltk import word_tokenize, sent_tokenize
+#
+# message = "I will walk 500 miles and I would walk 500 more, " \
+#        "just to be the man who walks a thousand miles to fall down at your door!"
+#
+# # tokenization
+# print(word_tokenize(message))
+# print(sent_tokenize(message))
 
 # import pandas as pd
 
-# reading csv file
+# # reading csv file
 # df = pd.read_csv('venv/resource/DataSet.csv')
 
-# read a specific location
+# # read a specific location
 # print(df.iloc[2, 3])
 
-# read each row
+# # read each row
 # print(df.iloc[1])
 # print(df.iloc[1:4])
 
-# read each column
+# # read each column
 # print(df['Calories'])
 # print(df['Calories'][0:5])
 # print(df[['Calories', 'Duration']])
